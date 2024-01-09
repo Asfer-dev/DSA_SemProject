823bbbe026734cff9ae80030b91bdcd7c20ceb9c
Mohammad Anas Siddiqui <MoAnasSiddiqui@users.noreply.github.com>
2024-01-09 18:41:49 +0500

    Added Menu
    Fixed Matrix Printing
    Added Dijkstra
    Added Prims
    Removed unnecessary code

--- countries.cpp
+++ countries.cpp
@@ -4,6 +4,9 @@
 #include <cmath>
 #include <fstream>
 #include <sstream>
+#include <queue>
+#include <stack>
+#include <omp.h>
 
 using namespace std;
 
@@ -32,30 +35,6 @@ struct Node
     this->longitude = longitude;
     this->population = population;
     this->adjacentCountries = adjacentCountries;
-  }
-};
-
-struct LinkedList
-{
-  Node *countryNode;
-  LinkedList *next;
-  LinkedList(Node node)
-  {
-    this->countryNode = &node;
-    next = this;
-  }
-  LinkedList(Node node, LinkedList *end)
-  {
-    this->countryNode = &node;
-    if (end != NULL)
-    {
-      this->next = end;
-      end->next = this;
-    }
-    else
-    {
-      this->next = this;
-    }
   }
 };
 
@@ -89,18 +68,20 @@ double haversineDistance(double lat1, double lon1, double lat2, double lon2)
 
   return distance;
 }
+int findCountry(vector<Node> nodes, int start, int end, string key);
 
 class Graph
 {
 public:
   vector<vector<int>> adjacencyMatrix;
+  int numberOfNodes;
   Graph(vector<Node> &nodes, vector<tuple<int, int, int>> &weightedEdges)
   {
+    numberOfNodes = nodes.size();
     // Initialize the adjacency matrix with infinity (assuming no direct connection)
     for (int i = 0; i < nodes.size(); ++i)
     {
-      vector<int> row(nodes.size(), -1);
-      row[i] = 0; // Set diagonal elements to 0
+      vector<int> row(nodes.size(), INT_MAX);
       adjacencyMatrix.push_back(row);
     }
 
@@ -116,22 +97,215 @@ public:
     }
   }
 
-  void displayMatrix() const
+  void displayMatrix(vector<Node> nodes)
   {
-    int i = 0;
-    for (const auto &row : adjacencyMatrix)
+    for (int i = 0; i < adjacencyMatrix.size(); i++)
     {
-      if (i > 9)
-        break;
-      for (int val : row)
+      cout << nodes[i].name << endl;
+      for (int j = 0; j < adjacencyMatrix.size(); j++)
       {
-        cout << (val == -1 ? "INF" : to_string(val)) << " ";
+        if (adjacencyMatrix[i][j] != INT_MAX)
+        {
+          cout << nodes[j].name << ": " << to_string(adjacencyMatrix[i][j]) << "km -- ";
+        }
       }
-      cout << endl;
-      i++;
+      cout << "N/A" << endl
+           << endl;
     }
   }
+
+  void bfsTraversal(int vertex)
+  {
+    if (vertex >= numberOfNodes || vertex < 0)
+    {
+      return;
+    }
+    vector<bool> visited(numberOfNodes, false);
+    queue<int> q;
+    q.push(vertex);
+    while (q.empty() == false)
+    {
+      vertex = q.front();
+      q.pop();
+      if (visited[vertex] == false)
+      {
+        visited[vertex] = true;
+        cout << vertex << " ";
+        for (int i = 0; i < numberOfNodes; i++)
+        {
+          if (adjacencyMatrix[vertex][i] == true)
+          {
+            q.push(i);
+          }
+        }
+      }
+    }
+  }
+  void dfsTraversal(int vertex)
+  {
+    if (vertex >= numberOfNodes || vertex < 0)
+    {
+      return;
+    }
+    vector<bool> visited(numberOfNodes, false);
+    stack<int> s;
+    s.push(vertex);
+    while (s.empty() == false)
+    {
+      vertex = s.top();
+      s.pop();
+      if (visited[vertex] == false)
+      {
+        visited[vertex] = true;
+        cout << vertex << " ";
+        for (int i = 0; i < numberOfNodes; i++)
+        {
+          if (adjacencyMatrix[vertex][i] == true)
+          {
+            s.push(i);
+          }
+        }
+      }
+    }
+  }
+  void printPrims(int parent[], int distance[], vector<Node> nodes)
+  {
+    cout << "Node  :  Parent  :  Weight" << endl;
+    for (int i = 0; i < numberOfNodes; i++)
+    {
+      if (distance[i] != INT_MAX)
+      {
+        cout << nodes[i].name << " :: " << nodes[parent[i]].name << " :: " << distance[i] << endl;
+      }
+    }
+  }
+  void printDijkstra(int parent[], vector<Node> nodes)
+  {
+    int temp;
+    string destination;
+    string path = "";
+    cout << "Enter your destination country: ";
+    getline(cin >> ws, destination);
+    cout << endl
+         << "Source :: ";
+    temp = findCountry(nodes, 0, nodes.size() - 1, destination);
+    int counter = 0;
+    while (temp != -1)
+    {
+      counter++;
+      path = nodes[temp].name + "  " + path;
+      temp = parent[temp];
+    }
+    if (counter == 0)
+    {
+      cout << "The countries are not accessible by road ";
+    }
+    else
+    {
+      cout << path;
+    }
+    cout << ":: Destination" << endl;
+  }
+  int minimumUnvisitedNode(int distance[], bool visited[])
+  {
+    int minimum;
+    int dist = INT_MAX;
+    for (int i = 0; i < numberOfNodes; i++)
+    {
+      if (distance[i] <= dist && visited[i] == false)
+      {
+        dist = distance[i], minimum = i;
+      }
+    }
+    return minimum;
+  }
+  void dijkstra(int source, vector<Node> nodes)
+  {
+    int distance[numberOfNodes];
+    bool visited[numberOfNodes];
+    int parent[numberOfNodes];
+
+    for (int i = 0; i < numberOfNodes; i++)
+    {
+      distance[i] = INT_MAX, visited[i] = false, parent[i] = -1;
+    }
+
+    distance[source] = 0;
+
+    for (auto i = 0; i < numberOfNodes - 1; i++)
+    {
+      int vertex = minimumUnvisitedNode(distance, visited);
+
+      visited[vertex] = true;
+
+      for (int j = 0; j < numberOfNodes; j++)
+      {
+        if (visited[j] == false && adjacencyMatrix[vertex][j] != INT_MAX && distance[vertex] != INT_MAX && distance[vertex] + adjacencyMatrix[vertex][j] < distance[j])
+        {
+          distance[j] = distance[vertex] + adjacencyMatrix[vertex][j];
+          parent[j] = vertex;
+        }
+      }
+    }
+    printDijkstra(parent, nodes);
+  }
+  void prims(int source, vector<Node> nodes)
+  {
+    int distance[numberOfNodes];
+    bool visited[numberOfNodes];
+    int parent[numberOfNodes];
+
+    for (int i = 0; i < numberOfNodes; i++)
+    {
+      distance[i] = INT_MAX, visited[i] = false, parent[i] = -1;
+    }
+
+    distance[source] = 0;
+
+    for (auto i = 0; i < numberOfNodes - 1; i++)
+    {
+      int vertex = minimumUnvisitedNode(distance, visited);
+
+      visited[vertex] = true;
+
+      for (int j = 0; j < numberOfNodes; j++)
+      {
+        if (visited[j] == false && adjacencyMatrix[vertex][j] != INT_MAX && adjacencyMatrix[vertex][j] < distance[j])
+        {
+          distance[j] = adjacencyMatrix[vertex][j];
+          parent[j] = vertex;
+        }
+      }
+    }
+    printPrims(parent, distance, nodes);
+  }
 };
+
+int findCountry(vector<Node> nodes, int start, int end, string key)
+{
+  if (start > end)
+  {
+    return -1;
+  }
+
+  else
+  {
+    int mid = (start + end) / 2;
+    // cout << start << " " << end << endl;
+    if (nodes[mid].name.compare(key) == 0)
+    {
+      return mid;
+    }
+    else if (nodes[mid].name.compare(key) > 0)
+    {
+      return findCountry(nodes, start, mid + 1, key);
+    }
+    else
+    {
+      return findCountry(nodes, mid - 1, end, key);
+    }
+  }
+}
 
 int main()
 {
@@ -276,5 +450,66 @@ int main()
   }
 
   Graph countriesGraph(nodes, weightedEdges);
-  countriesGraph.displayMatrix(); // only prints the first 10 rows for simplicity
+
+  int option;
+  while (true)
+  {
+    cout << "----------------------------------------------------------" << endl;
+    cout << "Menu: " << endl;
+    cout << "1: Print country data: " << endl;
+    cout << "2: BFS Traversal: " << endl;
+    cout << "3: DFS Traversal: " << endl;
+    cout << "4: Dijkstra Algorithm (Shortest Path): " << endl;
+    cout << "5: Prim's Algorithm (Minimum Spanning Tree): " << endl;
+    cout << "0: Exit: " << endl
+         << endl;
+    cout << "Enter: ";
+    cin >> option;
+    cout << "----------------------------------------------------------" << endl;
+    if (option == 1)
+    {
+      countriesGraph.displayMatrix(nodes);
+    }
+    if (option == 2)
+    {
+      cout << "Enter starting point: ";
+      int vertex;
+      cin >> vertex;
+      cout << "BFS traversal: ";
+      countriesGraph.bfsTraversal(vertex);
+      cout << "...End..." << endl;
+    }
+    if (option == 3)
+    {
+      cout << "Enter starting point: ";
+      int vertex;
+      cin >> vertex;
+      cout << "DFS traversal: ";
+      countriesGraph.dfsTraversal(vertex);
+      cout << "...End..." << endl;
+    }
+    if (option == 4)
+    {
+      string source;
+      cout << "Enter your source country: ";
+      getline(cin >> ws, source);
+
+      int temp = findCountry(nodes, 0, nodes.size() - 1, source);
+
+      countriesGraph.dijkstra(temp, nodes);
+    }
+    if (option == 5)
+    {
+      string source;
+      cout << "Enter source country: ";
+      getline(cin >> ws, source);
+      int temp = findCountry(nodes, 0, nodes.size() - 1, source);
+      countriesGraph.prims(temp, nodes);
+    }
+    if (option == 0)
+    {
+      break;
+    }
+  }
+  return 0;
 }
